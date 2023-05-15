fb7a78666f8cf4fe984b6220c2c4f1e4d9a144a0
Ahnoobs <ademirj93@gmail.com>
2023-05-14 23:03:07 -0300

    update: Adjustments of algorithm

--- ex_1_iris.py
+++ ex_1_iris.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
 
 # Carregando o dataset wine
 data = load_iris()
@@ -13,42 +14,52 @@ y = data.target
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-# Dividindo os dados em conjunto de treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.15, random_state=42)
+# Separa os dados em treinamento, validação e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5)
 
 # Função de ativação
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-
-def softmax(X):
-    exp_scores = np.exp(X - np.max(X, axis=1, keepdims=True))
-    return exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-
 # Classe do perceptron
 class Perceptron:
-    def __init__(self, lr=0.5, epochs=100):
+    def __init__(self, lr=0.5, epochs=500):
         self.lr = lr
         self.epochs = epochs
-        
-    def fit(self, X, y):
+    
+    #Treinamento da rede    
+    def fit(self, X, y,X_val=None, y_val=None):
         self.weights = np.zeros(X.shape[1])
         self.bias = 0
-        
+        self.X_val = X_val
+        self.y_val = y_val
+
+        val_errors = [] 
         errors = []
         for i in range(self.epochs):
             error = 0
             for xi, yi in zip(X, y):
                 y_pred = sigmoid(np.dot(xi, self.weights) + self.bias)
                 
+                #Variavéis para depuração da atualização de pesos
                 delta_w = self.lr * (yi - y_pred) * xi
                 delta_b = self.lr * (yi - y_pred)
-  
+                #Atualização dos pesos
                 self.weights += delta_w                
                 self.bias += delta_b
                 error += (yi - y_pred) ** 2
             errors.append(error)
-        return errors
+
+            # Cálculo do erro de validação
+            if X_val is not None and y_val is not None:
+                val_error = 0
+                for xi, yi in zip(X_val, y_val):
+                    y_val_pred = sigmoid(np.dot(xi, self.weights) + self.bias)
+                    val_error += (yi - y_val_pred) ** 2
+                val_errors.append(val_error)
+
+        return errors, val_errors
 
           
 
@@ -60,7 +71,7 @@ class Perceptron:
 perceptron = Perceptron(lr=0.1, epochs=100)
 
 # Treinando o perceptron
-errors = perceptron.fit(X_train, y_train)
+errors, val_errors = perceptron.fit(X_train, y_train, X_val, y_val)
 
 # Predições do modelo
 y_pred_train = perceptron.predict(X_train)
@@ -71,9 +82,9 @@ accuracy_train = np.mean(y_pred_train == y_train)
 accuracy_test = np.mean(y_pred_test == y_test)
 
 # Plotagem dos resultados
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 5))
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(15, 10))
 ax1.plot(errors)
-ax1.set_title('Erro Quadrático')
+ax1.set_title('Erro Quadrático de treinamento')
 ax1.set_xlabel('Épocas')
 ax1.set_ylabel('Erro')
 ax2.scatter(X_train[:,0], X_train[:,1], c=y_train)
@@ -84,6 +95,10 @@ ax3.scatter(X_test[:,0], X_test[:,1], c=y_test)
 ax3.set_title('Teste')
 ax3.set_xlabel('Característica 1')
 ax3.set_ylabel('Característica 2')
+ax4.plot(val_errors)
+ax4.set_title('Erro Quadrático de validação')
+ax4.set_xlabel('Épocas')
+ax4.set_ylabel('Erro')
 plt.show()
 
 # Imprimindo os resultados
@@ -91,10 +106,31 @@ print('Erro Quadrático:', errors[-1])
 print('Acurácia (Treinamento):', accuracy_train)
 print('Acurácia (Teste):', accuracy_test)
 
-# softmax results
-softmax_scores = softmax(X_test[:10])
-#print("Softmax results:")
-#print(softmax_scores)
+#Matriz de Confusão
+cmteste = confusion_matrix(y_test, y_pred_test)
 
-for i in softmax_scores:
-    print (np.sum(i))
+# Extrair os valores da matriz de confusão
+tn = cmteste[0,0]
+fp = cmteste[0,1]
+fn = cmteste[1,0]
+tp = cmteste[1,1]
+
+# Imprimir os valores
+print("Matriz de Confusão de teste x Validação:")
+print("Verdadeiros Positivos:", tp)
+print("Verdadeiros Negativos:", tn)
+print("Falsos Positivos:", fp)
+print("Falsos Negativos:", fn)
+
+# Plota a matriz de confusão do teste X
+plt.imshow(cmteste, cmap='Blues')
+plt.title('Matriz de confusão x validação')
+plt.colorbar()
+plt.xlabel('Predição')
+plt.ylabel('Real')
+plt.xticks(np.arange(len(data.target_names)), data.target_names)
+plt.yticks(np.arange(len(data.target_names)), data.target_names)
+for i in range(len(data.target_names)):
+    for j in range(len(data.target_names)):
+        plt.text(j, i, cmteste[i, j], ha='center', va='center')
+plt.show()
